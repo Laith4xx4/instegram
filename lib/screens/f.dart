536078db03a19bacfa2a2955eb7e4df065b582c1
Laith4xx4 <laithyassen02@gmail.com>
2025-10-13 18:24:21 +0300

    Initial project commit

---
+++ lib/screens/f.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class F extends StatefulWidget {
+  const F({super.key});
+
+  @override
+  State<F> createState() => _FState();
+}
+
+class _FState extends State<F> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
